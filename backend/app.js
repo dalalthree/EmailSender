4b04ba22080f7e936cd812fc58b4f32386887cb0
unknown <ryanadalal@gmail.com>
2022-01-15 16:24:17 -0500

    initial commit

---
+++ backend/app.js
@@ -0,0 +1,81 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+var cors = require("cors");
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+var testAPIRouter = require("./routes/testBackend");
+var MongoClient = require('mongodb').MongoClient;
+var nodemailer = require('nodemailer');
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+app.use(cors());
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+app.use("/testBackend", testAPIRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+var url = "mongodb+srv://dalalthree:d!Aw*tDJ-6HDTD$@cluster0.euxda.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+
+MongoClient.connect(url, function(err, db) {
+  if (err) throw err;
+  var dbo = db.db("mydb");
+  var myobj = { name: "Co Inc", address: "Road 3" };
+  dbo.collection("customers").insertOne(myobj, function(err, res) {
+    if (err) throw err;
+    console.log("1 document inserted");
+    db.close();
+  });
+});
+
+var transporter = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: 'dalalryan717@gmail.com',
+    pass: 'Yankees#42'
+  }
+});
+  
+var mailOptions = {
+  from: 'dalalryan717@gmail.com',
+  to: 'ryanadalal@gmail.com',
+  subject: 'Sending Email using Node.js',
+  text: 'That was easy!'
+};
+transporter.sendMail(mailOptions, function(error, info){
+  if (error) {
+    console.log(error);
+  } else {
+    console.log('Email sent: ' + info.response);
+  }
+});
+
+module.exports = app;
